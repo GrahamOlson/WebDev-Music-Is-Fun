6d810830e7e1f73ee5e9bb892c960939167ebf55
Graham Olson <olsongraham32@gmail.com>
2018-08-15 20:53:34 -0600

    added styling

--- itunes-controller.js
+++ itunes-controller.js
@@ -4,7 +4,7 @@ function ItunesController() {
   const itunesService = new ItunesService()
 
   function drawSongs(results) {
-    console.log(results)
+
     let template = ''
     for (let i = 0; i < results.length; i++) {
       const song = results[i];
@@ -12,16 +12,12 @@ function ItunesController() {
       template += `
       <div class="col-6">  
       <ul style="list-style-type:none" id="song-list">
-        <li class="font-weight-bold title">${song.title}</li>
-        <li><img src=${song.albumArt}></li>
-        <li>${song.artist}</li>
-        <li>${song.collection}</li>
-        <li>${song.price}</li>
-        <audio controls>
-            <source src="${song.preview}" type="audio/ogg">
-            <source src="${song.preview}" type="audio/mpeg">  
-        <li>${song.preview}</li>
-        </audio>
+        <li class="font-weight-bold song-title"> ${song.title}</li>
+        <li><button onclick="playAudio()"type="button"><img src=${song.albumArt}></button></li>
+        <li class="song-artist">${song.artist}</li>
+        <li class="song-collection">${song.collection}</li>
+        <li class="song-price">$${song.price}</li>
+        
       </ul>
       </div>
     `
@@ -41,6 +37,21 @@ function ItunesController() {
       //changes button back to GET MUSIC once songs are loaded
       $('#get-music-button').text('GET MUSIC');
     })
+  }
+
+  this.playAudio = function (results) {
+    let template = '';
+    for (let i = 0; i < results.length; i++) {
+      const song = results[i];
+      template += `
+      <audio controls>
+            <source src="${song.preview}" type="audio/ogg">
+            <source src="${song.preview}" type="audio/mpeg">  
+      </audio>
+      `
+      document.getElementById('audio-player').innerHTML = template
+    }
+
   }
 
 
